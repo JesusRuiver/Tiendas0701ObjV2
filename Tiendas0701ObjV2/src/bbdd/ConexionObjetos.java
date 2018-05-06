436d0b3db1f7a6a47f1049053e7914ad6ec8d0c7
Jesús <jesusruiver@jesusruiver.com>
2018-05-06 12:36:14 +0200

    Primer commit

---
+++ Tiendas0701ObjV2/src/bbdd/ConexionObjetos.java
@@ -0,0 +1,360 @@
+package bbdd;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ConexionObjetos {
+
+	private Connection conexion;
+	private String usuario;
+	private String contraseña;
+	private String baseDatos;
+	private String servidor;
+	private String conectorJDBC;
+
+	private ResultSet resultado;
+	private Statement sentencia;
+
+	public ConexionObjetos() {
+
+	}
+
+	public ConexionObjetos(Connection conexion, String usuario, String contraseña, String baseDatos, String servidor,
+			String conectorJDBC) {
+		this.conexion = conexion;
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+		this.baseDatos = baseDatos;
+		this.servidor = servidor;
+		this.conectorJDBC = conectorJDBC;
+	}
+
+	public void conectar() {
+
+		this.conectorJDBC = "com.mysql.jdbc.Driver";
+		this.servidor = "jdbc:mysql://localhost/";
+		this.baseDatos = "tiendas";
+		this.usuario = "tiendas";
+		this.contraseña = "tiendas";
+
+		try {
+
+			Class.forName(getConectorJDBC());
+
+			this.conexion = DriverManager.getConnection(getServidor() + getBaseDatos(), getUsuario(), getContraseña());
+
+			this.sentencia = conexion.createStatement();
+
+			System.out.println("Conectado");
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error... No Conectado");
+			e.printStackTrace();
+		}
+
+	}
+
+	public void cerrarConexion() {
+		try {
+			conexion.close();
+			
+			System.out.println("La conexión se ha cerrado correctamente");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			System.out.println("Error al cerrar la conexión");
+		}
+	}
+
+	public ArrayList<Tienda> dameTiendas() {
+
+		ArrayList<Tienda> lista = new ArrayList<Tienda>();
+		String consulta = "SELECT * FROM tiendas";
+
+		try {
+			resultado = this.sentencia.executeQuery(consulta);
+
+			System.out.println("Correcto");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error");
+		}
+
+		try {
+			while (resultado.next()) {
+
+				Tienda tiendas = new Tienda();
+
+				tiendas.setNombre(resultado.getString(2));
+				tiendas.setNif(resultado.getString(1));
+
+				lista.add(tiendas);
+
+			}
+			resultado.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return lista;
+
+	}
+
+	public ArrayList<Articulo> dameArticulos() {
+
+		ArrayList<Articulo> listaArticulos = new ArrayList<Articulo>();
+
+		String consulta = "Select distinct articulos.articulo, fabricantes.nombre from articulos, fabricantes where articulos.cod_fabricante = fabricantes.cod_fabricante";
+
+		try {
+			resultado = this.sentencia.executeQuery(consulta);
+			System.out.println("Correcto");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error");
+		}
+
+		try {
+			while (resultado.next()) {
+
+				Articulo articulo = new Articulo();
+
+				articulo.setNombreArticulo(resultado.getString(1));
+				articulo.setCodFabricante(resultado.getInt(2));
+
+				listaArticulos.add(articulo);
+			}
+			resultado.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return listaArticulos;
+
+	}
+
+	public ArrayList<Venta> rellenaTablaVentas(String nif) {
+
+		ArrayList<Venta> datos = new ArrayList<Venta>();
+
+		PreparedStatement enviaConsultaArticulosVentas;
+		String consultaPreparadaArticulosVentas = "select v.nif, v.articulo, f.nombre, v.peso, v.categoria, v.fecha_venta, "
+				+ "v.unidades_vendidas as 'Unidades Vendidas', a.precio_venta, (v.unidades_vendidas * a.precio_venta) as 'Total Uds. Ventas' "
+				+ "from ventas v inner join articulos a on v.articulo = a.articulo and v.categoria = a.categoria, fabricantes f "
+				+ "where nif=? and v.cod_fabricante = f.cod_fabricante";
+
+		try {
+
+			enviaConsultaArticulosVentas = conexion.prepareStatement(consultaPreparadaArticulosVentas);
+			enviaConsultaArticulosVentas.setString(1, nif);
+
+			resultado = enviaConsultaArticulosVentas.executeQuery();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			while (resultado.next()) {
+
+				Venta filasVenta = new Venta();
+
+				filasVenta.setNif(resultado.getString(1));
+				filasVenta.setArticulo(resultado.getString(2));
+				filasVenta.setCodFabricante(resultado.getInt(3));
+				filasVenta.setPeso(resultado.getInt(4));
+				filasVenta.setCategoria(resultado.getString(5));
+				filasVenta.setFechaVenta(resultado.getString(6));
+				filasVenta.setUnidadesVendidas(resultado.getInt(7));
+
+
+
+
+				datos.add(filasVenta);
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return datos;
+
+	}
+
+	public ArrayList<Pedido> rellenaTablaPedidos(String nif) {
+
+		ArrayList<Pedido> datos = new ArrayList<Pedido>();
+
+		PreparedStatement enviaConsultaArticulosPedidos;
+
+		String consultaPreparadaArticulosPedido = "select p.nif, p.articulo, f.nombre, p.peso, p.categoria, p.fecha_pedido, "
+				+ "p.unidades_pedidas, a.precio_costo, (p.unidades_pedidas * a.precio_costo) as 'Total Uds. Pedidos' "
+				+ "from pedidos p inner join articulos a on p.articulo = a.articulo and p.categoria = a.categoria, fabricantes f "
+				+ "where nif=? and p.cod_fabricante = f.cod_fabricante";
+
+		try {
+
+			enviaConsultaArticulosPedidos = conexion.prepareStatement(consultaPreparadaArticulosPedido);
+
+			enviaConsultaArticulosPedidos.setString(1, nif);
+
+			resultado = enviaConsultaArticulosPedidos.executeQuery();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			while (resultado.next()) {
+
+				Pedido filasPedido = new Pedido();
+
+				filasPedido.setNif(resultado.getString(1));
+				filasPedido.setNombreArticulo(resultado.getString(2));
+				filasPedido.setCodFabricante(resultado.getString(3));
+				filasPedido.setPeso(resultado.getInt(4));
+				filasPedido.setCategoria(resultado.getString(5));
+				filasPedido.setFechaPedido(resultado.getString(6));
+				filasPedido.setUnidadesPedidas(resultado.getInt(7));
+				filasPedido.setPrecioCosto(resultado.getInt(8));
+
+				filasPedido.setTotalUnidadesPedidas(resultado.getInt("Total Uds. Pedidos"));
+
+				datos.add(filasPedido);
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return datos;
+	}
+
+	public String sumaPrecioCosto(String nif) {
+
+		String totalPedidos = null;
+
+		PreparedStatement enviaConsultaSumaPedidos;
+
+		String consultaPreparadaSumaPedidos = "select sum(p.unidades_pedidas * a.precio_costo) as 'Total Uds. Pedidas' "
+				+ "from pedidos p inner join articulos a on p.articulo = a.articulo and p.categoria = a.categoria, fabricantes f "
+				+ "where nif=? and p.cod_fabricante = f.cod_fabricante;";
+
+		try {
+
+			enviaConsultaSumaPedidos = conexion.prepareStatement(consultaPreparadaSumaPedidos);
+			enviaConsultaSumaPedidos.setString(1, nif);
+
+			resultado = enviaConsultaSumaPedidos.executeQuery();
+
+			if (resultado.next()) {
+
+				totalPedidos = resultado.getString("Total Uds. Pedidas");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return totalPedidos;
+	}
+
+	public String sumaPrecioVenta(String nif) {
+
+		String totalVentas = "";
+
+		PreparedStatement enviaConsultaSumaVentas;
+
+		String consultaPreparadaSumaVentas = "select sum(v.unidades_vendidas * a.precio_venta) as 'Total Uds. Vendidas' "
+				+ "from ventas v inner join articulos a on v.articulo = a.articulo and v.categoria = a.categoria, fabricantes f "
+				+ "where nif=? and v.cod_fabricante = f.cod_fabricante";
+
+		try {
+			enviaConsultaSumaVentas = conexion.prepareStatement(consultaPreparadaSumaVentas);
+
+			enviaConsultaSumaVentas.setString(1, nif);
+
+			resultado = enviaConsultaSumaVentas.executeQuery();
+
+			if (resultado.next()) {
+				totalVentas = resultado.getString("Total Uds. Vendidas");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return totalVentas;
+	}
+
+	public ObjectOutputStream exportarFicheroBinario() throws IOException {
+
+		FileOutputStream fichero = new FileOutputStream("FicheroBinaro.dat");
+
+		ObjectOutputStream fileout = new ObjectOutputStream(fichero);
+
+		return fileout;
+
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getBaseDatos() {
+		return baseDatos;
+	}
+
+	public void setBaseDatos(String baseDatos) {
+		this.baseDatos = baseDatos;
+	}
+
+	public String getServidor() {
+		return servidor;
+	}
+
+	public void setServidor(String servidor) {
+		this.servidor = servidor;
+	}
+
+	public String getConectorJDBC() {
+		return conectorJDBC;
+	}
+
+	public void setConectorJDBC(String conectorJDBC) {
+		this.conectorJDBC = conectorJDBC;
+	}
+
+}
---
+++ Tiendas0701ObjV2/src/gui/Ejercicio1Objetos.java
@@ -0,0 +1,278 @@
+package gui;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+
+import bbdd.ConexionObjetos;
+import bbdd.Pedido;
+import bbdd.Tienda;
+import bbdd.Venta;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Ejercicio1Objetos extends JFrame {
+
+	private JPanel contentPane;
+	private JTable tablaVentas;
+	private JTable tablaPedidos;
+	private JLabel lbResultadoTotal = new JLabel("");
+	private JScrollPane scrollPaneVentas;
+	private JScrollPane scrollPanePedidos;
+	private JComboBox<Tienda> cboxTiendas;
+
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	private JRadioButton rbtnVentas = new JRadioButton("Ventas");
+	private JRadioButton rbtnPedidos = new JRadioButton("Pedidos");
+
+	private ConexionObjetos miConexion = new ConexionObjetos();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ejercicio1Objetos frame = new Ejercicio1Objetos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * Create the frame. //Constructor de la Ventana
+	 */
+	public Ejercicio1Objetos() {
+
+		miConexion.conectar();
+
+		/*------------------------COMPONENTES DE LA VENTANA----------------------------*/
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 892, 611);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		cboxTiendas = new JComboBox<Tienda>();
+		cboxTiendas.setBounds(33, 22, 262, 26);
+		contentPane.add(cboxTiendas);
+
+		buttonGroup.add(rbtnVentas);
+
+		rbtnVentas.setBounds(65, 66, 77, 23);
+		contentPane.add(rbtnVentas);
+
+		buttonGroup.add(rbtnPedidos);
+
+		rbtnPedidos.setBounds(65, 92, 77, 23);
+		contentPane.add(rbtnPedidos);
+
+		JLabel lbTotal = new JLabel("Total:");
+		lbTotal.setFont(new Font("Arial", Font.BOLD, 15));
+		lbTotal.setBounds(330, 528, 46, 14);
+		contentPane.add(lbTotal);
+		lbResultadoTotal.setFont(new Font("Tahoma", Font.PLAIN, 14));
+
+		lbResultadoTotal.setBounds(386, 521, 214, 26);
+		contentPane.add(lbResultadoTotal);
+
+		scrollPaneVentas = new JScrollPane();
+		scrollPaneVentas.setBounds(33, 127, 801, 374);
+		contentPane.add(scrollPaneVentas);
+
+		scrollPanePedidos = new JScrollPane();
+		scrollPanePedidos.setBounds(33, 127, 801, 374);
+		contentPane.add(scrollPanePedidos);
+
+		/*---------------------------------ACCIONES DE LOS BOTONES----------------------*/
+
+		rellenaComboBox(cboxTiendas);
+
+		rbtnVentas.setSelected(true);
+
+		rbtnVentas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				String nif = seleccionaNifComboTiendas();
+
+				String resultadoTotalVentas = miConexion.sumaPrecioVenta(nif);
+
+				construirTablaVentas(nif);
+
+				lbResultadoTotal.setText(resultadoTotalVentas + " € Ingresos Ventas");
+
+			}
+		});
+
+		rbtnPedidos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				String nif = seleccionaNifComboTiendas();
+
+				String resultadoTotalPedidos = miConexion.sumaPrecioCosto(nif);
+
+				construirTablaPedidos(nif);
+
+				lbResultadoTotal.setText(resultadoTotalPedidos + "€ Coste Pedidos");
+
+			}
+		});
+
+		cboxTiendas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				String nif = seleccionaNifComboTiendas();
+
+				String resultadoTotalVentas = miConexion.sumaPrecioVenta(nif);
+
+				String resultadoTotalPedidos = miConexion.sumaPrecioCosto(nif);
+
+				if (rbtnVentas.isSelected() == true) {
+
+					construirTablaVentas(nif);
+
+					lbResultadoTotal.setText(resultadoTotalVentas + " € Ingresos Ventas");
+
+				} else {
+
+					construirTablaPedidos(nif);
+
+					lbResultadoTotal.setText(resultadoTotalPedidos + "€ Coste Pedidos");
+
+					System.out.println("Esta seleccionando pedidos");
+				}
+
+			}
+
+		});
+
+		/* Al cerrar la ventana se cierra la conexión */
+
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+
+				miConexion.cerrarConexion();
+			}
+		});
+
+	}
+
+	/*-------------------------------------METODOS-----------------------------------*/
+
+	private String seleccionaNifComboTiendas() {
+		
+		Tienda t1 = new Tienda();
+		
+		t1 = (Tienda) cboxTiendas.getSelectedItem();
+		
+		return t1.getNif();
+		
+	}
+
+	private void construirTablaVentas(String nif) {
+
+		String titulosColumnas[] = { "NIF", "ARTICULO", "FABRICANTE", "PESO", "CATEGORIA", "FECHA VENTA",
+				"UNIDADES VENDIDAS", "PRECIO VENTA", "TOTAL VENTAS" };
+		String informacionTablaVentas[][] = obtenerDatosVentas(nif);
+
+		tablaVentas = new JTable(informacionTablaVentas, titulosColumnas);
+		scrollPaneVentas.setViewportView(tablaVentas);
+
+	}
+
+	private String[][] obtenerDatosVentas(String nif) {
+
+		ArrayList<Venta> datos = new ArrayList<Venta>();
+
+		datos = miConexion.rellenaTablaVentas(nif);
+
+		String matrizInfo[][] = new String[datos.size()][9];// matriz [fila]
+															// [columna]
+
+		for (int i = 0; i < datos.size(); i++) {
+
+			matrizInfo[i][0] = datos.get(i).getNif();
+			matrizInfo[i][1] = datos.get(i).getArticulo();
+			matrizInfo[i][2] = datos.get(i).getCodFabricante();
+			matrizInfo[i][3] = datos.get(i).getPeso() + "";
+			matrizInfo[i][4] = datos.get(i).getCategoria();
+			matrizInfo[i][5] = datos.get(i).getFechaVenta();
+			matrizInfo[i][6] = datos.get(i).getUnidadesVendidas() + "";
+			matrizInfo[i][7] = datos.get(i).getPrecioVenta() + "";
+			matrizInfo[i][8] = datos.get(i).getTotalUnidadesVendidas() + "";
+		}
+
+		return matrizInfo;
+	}
+
+	private void construirTablaPedidos(String nif) {
+
+		String titulosColumnas[] = { "NIF", "ARTICULO", "FABRICANTE", "PESO", "CATEGORIA", "FECHA PEDIDO",
+				"UNIDADES PEDIDAS", "PRECIO COSTO", "TOTAL PEDIDOS" };
+		String informacionTablaPedidos[][] = obtenerDatosPedidos(nif);
+
+		tablaPedidos = new JTable(informacionTablaPedidos, titulosColumnas);
+		scrollPanePedidos.setViewportView(tablaPedidos);
+
+	}
+
+	private String[][] obtenerDatosPedidos(String nif) {
+
+		ArrayList<Pedido> datos = new ArrayList<Pedido>();
+
+		datos = miConexion.rellenaTablaPedidos(nif);
+
+		String matrinzInfo[][] = new String[datos.size()][9];
+
+		for (int i = 0; i < datos.size(); i++) {
+
+			matrinzInfo[i][0] = datos.get(i).getNif();
+			matrinzInfo[i][1] = datos.get(i).getNombreArticulo();
+			matrinzInfo[i][2] = datos.get(i).getCodFabricante();
+			matrinzInfo[i][3] = datos.get(i).getPeso() + "";
+			matrinzInfo[i][4] = datos.get(i).getCategoria();
+			matrinzInfo[i][5] = datos.get(i).getFechaPedido();
+			matrinzInfo[i][6] = datos.get(i).getUnidadesPedidas() + "";
+			matrinzInfo[i][7] = datos.get(i).getPrecioCosto() + "";
+			matrinzInfo[i][8] = datos.get(i).getTotalUnidadesPedidas() + "";
+
+		}
+
+		return matrinzInfo;
+
+	}
+
+	public void rellenaComboBox(JComboBox<Tienda> cboxTiendas) {
+		ArrayList<Tienda> lista = new ArrayList<Tienda>();
+
+		lista = miConexion.dameTiendas();
+
+		for (int i = 0; i < lista.size(); i++) {
+
+			cboxTiendas.addItem(lista.get(i));
+
+		}
+	}
+
+}
